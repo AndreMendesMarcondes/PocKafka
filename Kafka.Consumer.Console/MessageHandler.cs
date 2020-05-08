9b6a144b084dc211c1f910ae6fb0de76a7086dec
André Mendes Marcondes <andremendesmarcondes@gmail.com>
2020-05-07 23:04:16 -0300

    Add project files.

---
+++ Kafka.Consumer.Console/MessageHandler.cs
@@ -0,0 +1,49 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kafka.Consumer.Handler.Handler
+{
+    public class MessageHandler : IHostedService
+    {
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe("fila_pedido");
+                var cts = new CancellationTokenSource();
+
+                try
+                {
+                    while (true)
+                    {
+                        var message = c.Consume(cts.Token);
+                        System.Console.WriteLine($"Mensagem: {message.Value} recebida de {message.TopicPartitionOffset}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+
+            return Task.CompletedTask;
+
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Kafka.Consumer.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using Kafka.Consumer.Handler.Handler;
+using System.Threading;
+
+namespace Kafka.Consumer.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageHandler message = new MessageHandler();
+            message.StartAsync(CancellationToken.None);
+        }
+    }
+}
